fce52db4c95bf0b11815a1bdecc22ee95ff80fbf
Filipe Silvestre <fssilvestre5@gmail.com>
2015-08-08 13:27:31 -0700

    added Twig

--- app/app.php
+++ app/app.php
@@ -7,8 +7,17 @@
 
     // INITIALIZE APPLICATION
         $app = new Silex\Application();
-        
+        $app->register(new Silex\Provider\TwigServiceProvider(), array(
+            'twig.path' => __DIR__."/../views"
+        ));
     // ROUTES
+
+        // display index webpage
+        $app->get('/', function() use ($app) {
+
+            return $app['twig']->render('index.html.twig');
+        });
+
 
     return $app;
 
